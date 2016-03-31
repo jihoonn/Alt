a7b972f023e434290f7b799e0b9fcd3e6ba01d74
SeongHoo <sunghoo33@naver.com>
2016-03-31 23:27:37 +0900

    Added files via upload

---
+++ KSH/3x7+8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1=3;
+    int num2=7;
+    int num3=8;  
+    int result1=num1*num2+num3; //이 함수는 3x7+8의 값을 출력합니다.
+    
+    printf("%d*%d+%d=%d \n", num1, num2, num3, result1);
+    return 0;
+}
