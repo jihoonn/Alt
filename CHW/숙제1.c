261b8b9271abff551e2be48e76da72ae0eed2d09
lvbb102 <jugo1260@naver.com>
2016-04-03 23:37:15 +0900

    Added files via upload

---
+++ CHW/숙제1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1=3, num2=7, num3=8;  //세개의 정수입력 
+	int result;  
+	result=num1*num2+num3; //연산결과 
+	printf("%dX%d+%d=%d \n", num1, num2, num3, result); //연산결과출력 
+	return 0;
+}
+
+  //컴파일 성공 
