a42593c7c1b0cbcf834dc2e29aeff1b7c1f3c20f
SeongHoo <sunghoo33@naver.com>
2016-04-07 20:02:55 +0900

    Create p74_Q2_0407.c

---
+++ KSH/p74_Q2_0407.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1=3;
+    int num2=7;
+    int num3=8;  
+    int result1=29;
+    
+    scanf("%d*%d+%d=%d \n", num1, num2, num3, result1);
+    return 0;
+}
