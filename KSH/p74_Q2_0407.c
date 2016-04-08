f9cd9230e1631acbc51cb492e2407d8f927adc5b
SeongHoo <sunghoo33@naver.com>
2016-04-08 19:37:24 +0900

    Update p74_Q2_0407.c

--- KSH/p74_Q2_0407.c
+++ KSH/p74_Q2_0407.c
@@ -7,8 +7,6 @@ int main(void)
     int num3=8;  
     int result1=29;
     
-    scanf("%d*%d+%d=%d \n", num1, num2, num3, result1);
+    scanf("%d*%d+%d=%d \n", num1, num2, num3, result1); //계속 수정중
     return 0;
 }
-
-//계속 수정 중
