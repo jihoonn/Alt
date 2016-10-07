515f5ca84b0953ae9a2f74d194b934c662c59216
jihoonn <jihoon08142@gmail.com>
2016-10-07 23:59:58 +0900

    Add files via upload

---
+++ BJH/프로젝트.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int main()
+{
+	int opt;
+	
+	printf("뽑으신 숫자를 입력해주세요 : ");
+	scanf("%d", &opt);
+	
+	if(opt==69)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  2등  입니다. \n\n");
+	
+	if(opt==457)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==248)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==479)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==135)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==362)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==199) printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==176)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==318)
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	
+	if(opt==209)
+	{
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	}
+	if(opt=430)
+	{
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  3등  입니다. \n\n");
+	}
+	else
+	printf("\n당첨여부는 Enter키를 눌러주세요.\n\n");
+	getch();
+	
+	printf("  5등  입니다. ");
+	
+	main();
+	return ;
+	return 0;
+}
